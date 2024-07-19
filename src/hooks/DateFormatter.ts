d77f50ac7c9d525445ea77ff3becfe9925a944a9
KurbanTR <keshasemenov01@gmail.com>
2024-07-19 16:48:06 +0600

    YouTube Clone 1.0.4 - Добавил описание для видео

--- src/hooks/DateFormatter.ts
+++ src/hooks/DateFormatter.ts
@@ -1,12 +1,21 @@
-const DateFormatter = (dateString: string|number|undefined) => {
+const DateFormatter = (dateString: string | number | undefined) => {
+    if (dateString === undefined) {
+        return 'Invalid Date';
+    }
+
+    const date = new Date(dateString);
+    if (isNaN(date.getTime())) {
+        return 'Invalid Date';
+    }
+
     const formatDate = (date: Date) => {
         const now = new Date();
-        const diffTime = Math.abs(now.getTime() - date.getTime());
+        const diffTime = now.getTime() - date.getTime();
         const diffYears = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365));
-        const diffMonths = Math.floor((diffTime % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
-        const diffDays = Math.floor((diffTime % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
-        const diffHours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const diffMinutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+        const diffMonths = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 30)) % 12;
+        const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)) % 30;
+        const diffHours = Math.floor(diffTime / (1000 * 60 * 60)) % 24;
+        const diffMinutes = Math.floor(diffTime / (1000 * 60)) % 60;
 
         if (diffYears > 0) {
             return `${diffYears} г. назад`;
@@ -23,10 +32,6 @@ const DateFormatter = (dateString: string|number|undefined) => {
         }
     };
 
-    const date = new Date(dateString);
-    if (isNaN(date.getTime())) {
-        return 'Invalid Date';
-    }
     return formatDate(date);
 };
 
