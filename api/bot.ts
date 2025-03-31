c7d1344ed8af16ab966c0f3e5d24f7489da2d09c
KurbanTR <keshasemenov01@gmail.com>
2025-03-31 09:07:29 +0600

    add telegram app

---
+++ api/bot.ts
@@ -0,0 +1,21 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf('8020324555:AAFDbJGTwZrJwsLZnSA5GD4OdmGteBWRpZE');
+
+bot.command("start", (ctx) => {
+  ctx.reply("Открыть веб-приложение", {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "Открыть", web_app: { url: "https://you-tube-clone-delta-puce.vercel.app/"}}]
+      ],
+    },
+  });
+});
+
+export default async function handler(req: NowRequest, res: NowResponse) {
+  if (req.method === "POST") {
+    await bot.handleUpdate(req.body);
+  }
+  res.status(200).send("OK");
+}
