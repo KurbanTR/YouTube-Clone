d151068185a5d5d84e24919ce2e376f991d1f19b
KurbanTR <keshasemenov01@gmail.com>
2025-03-31 09:46:19 +0600

    fix

--- api/bot.ts
+++ api/bot.ts
@@ -1,21 +1,30 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-import { Telegraf } from "telegraf";
+import express from 'express';
+import { Telegraf } from 'telegraf';
 
+const app = express();
 const bot = new Telegraf('8020324555:AAFDbJGTwZrJwsLZnSA5GD4OdmGteBWRpZE');
 
-bot.command("web", (ctx) => {
-  ctx.reply("Открыть веб-приложение", {
+bot.start((ctx) => {
+  ctx.reply('Привет! Нажми кнопку для открытия веб-приложения', {
     reply_markup: {
       inline_keyboard: [
-        [{ text: "Открыть", web_app: { url: "https://t.me/MyYouTubeCloneBot/YouTubeClone" } }],
-      ],
-    },
+        [
+          {
+            text: 'Открыть веб-приложение',
+            web_app: { url: 'https://you-tube-clone-delta-puce.vercel.app' }
+          }
+        ]
+      ]
+    }
   });
 });
 
-export default async function handler(req: NowRequest, res: NowResponse) {
-  if (req.method === "POST") {
-    await bot.handleUpdate(req.body);
-  }
-  res.status(200).send("OK");
-}
+// Обрабатываем запросы от Telegram
+app.use('/api/bot', express.json(), async (req, res) => {
+  await bot.handleUpdate(req.body);
+  res.sendStatus(200); // Ответ Telegram о том, что данные получены
+});
+
+app.listen(5173, () => {
+  console.log('Server is running on port 5173');
+});
