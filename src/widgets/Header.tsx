f3b22fb1f7f19f71a313e65eb2f8563f4f9f6420
KurbanTR <keshasemenov01@gmail.com>
2025-04-02 17:42:33 +0600

    Header Styles For The Mobile

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import logo from '../assets/logo.svg';
 import search from '../assets/search.svg';
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Modal } from "antd";
 import '../styles/antdModel.css';
 import Catigories from "./Catigories";
@@ -9,6 +9,7 @@ import Catigories from "./Catigories";
 const Header: React.FC = () => {
     const [searchParams] = useSearchParams();
     const searchQuery = searchParams.get('search_query');
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const [query, setQuery] = useState('');
 
@@ -42,6 +43,14 @@ const Header: React.FC = () => {
         setIsModalOpen(false);
     };
 
+    useEffect(() => {
+        if (isModalOpen && inputRef.current) {
+            setTimeout(() => {
+                inputRef.current?.focus();
+            }, 100);
+        }
+    }, [isModalOpen]);
+
     return (
         <div className='fixed z-30 w-full bg-def-black overflow-auto'>
             <header className="flex justify-center items-center h-16 1650res:h-12 540res:px-3">
@@ -51,14 +60,14 @@ const Header: React.FC = () => {
                             <img src={logo} alt="logo" className="w-28 1650res:w-24 540res:w-20" />
                         </Link>
                     </div>
-                    <div className="w-[40%] 540res:hidden">
-                        <form onSubmit={handleSubmit} className="w-full border-[#808080] border-[1px] flex overflow-hidden items-center rounded-full h-11 1650res:h-10">
-                            <input type="text" value={query} onChange={handleInputChange} className="text-lg 1650res:text-base px-[1em] outline-none border-none bg-[var(--bg-color)] h-full text-white w-full" placeholder="Введите запрос" />
+                    <div className="w-[37em] 1650res:w-[25em] 540res:hidden">
+                        <form onSubmit={handleSubmit} className="w-full border-white border-[1px] border-opacity-20 flex overflow-hidden items-center rounded-full h-11 1650res:h-8">
+                            <input type="text" value={query} onChange={handleInputChange} className="text-[18px] font-sans 1650res:text-[14px] px-[1em] outline-none border-none bg-[var(--bg-color)] h-full text-white placeholder:text-white placeholder:text-opacity-35 w-full" placeholder="Введите запрос" />
                             {query !== '' && <div className="flex items-center">
-                                <img onClick={()=>setQuery('')} className="w-12 h-10 cursor-pointer rounded-full hover:bg-opacity-20 hover:bg-white" src="/cross.svg" alt="" />
+                                <img onClick={()=>setQuery('')} className="w-12 h-10 cursor-pointer rounded-full hover:bg-opacity-15 hover:bg-white" src="/cross.svg" alt="" />
                             </div>}
-                            <button className="flex justify-center items-center outline-none border-none h-full px-5 1650res:px-3 bg-[hsla(0,0%,100%,.04)] hover:bg-[hsla(0,0%,100%,.10)]">
-                                <img src={search} alt="search" className="w-8 1650res:w-7" />
+                            <button className="flex justify-center items-center outline-none border-l-[1px] border-white border-opacity-15 h-full px-5 1650res:px-3 bg-[hsla(0,0%,100%,.04)] hover:bg-[hsla(0,0%,100%,.10)]">
+                                <img src={search} alt="search" className="w-8 1650res:w-5" />
                             </button>
                         </form>
                     </div>
@@ -67,7 +76,7 @@ const Header: React.FC = () => {
                 </div>
                 <Modal className="hidden 540res:block" open={isModalOpen} onCancel={handleCancel} footer={null}>
                     <form onSubmit={handleSubmit}>
-                        <input type="text" value={query} onChange={handleInputChange} className="text-[1em] px-[0.5em] outline-none border-none border-2 bg-[var(--bg-color)] h-full text-white bg-black w-11/12 p-1 rounded-md" placeholder="Введите запрос" />
+                        <input type="text" ref={inputRef} value={query} onChange={handleInputChange} className="text-[1em] px-[0.5em] outline-none border-none border-2 bg-[var(--bg-color)] h-full text-white bg-black w-full p-1 rounded-md placeholder:text-white placeholder:text-opacity-35" placeholder="Введите запрос" />
                     </form>
                 </Modal>
             </header>
